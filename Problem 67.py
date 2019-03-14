9e40d82b1ff59571470dbebe92de8ab354a4ffca
Rohan Yadav <drohanyadav@gmail.com>
2019-03-14 17:22:41 +0530

    Add files via upload

---
+++ Problem 67.py
@@ -0,0 +1,26 @@
+import time
+import numpy as np
+start = time.time()  
+
+f = open(r'C:\Users\Gray\Documents\Python Scripts\p067_triangle.txt')
+
+d ={}
+
+for k,i in enumerate(f.readlines()):
+	i = i.split()
+	i = [int(j) for j in i]
+	d[k] = np.array(i)
+	n = k
+	
+n = [i for i in range(n,0,-1)]
+	
+for i in n:
+	temp = []
+	for k,j in enumerate(d[i]):
+		if k < len(d[i]) - 1: temp.append(max(d[i][k],d[i][k+1]))
+	d[i-1] = d[i-1] + np.array(temp)
+
+print(d[0])
+
+end = time.time()
+print(end-start)
