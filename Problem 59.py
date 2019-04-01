23a80190e6dc88b40f5dc67b55283f914996bd5f
Rohan Yadav <drohanyadav@gmail.com>
2019-04-01 17:31:06 +0530

    Add files via upload

---
+++ Problem 59.py
@@ -0,0 +1,47 @@
+import time
+from collections import Counter
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+      start = time.time()
+      result = func(*args, **kwargs)
+      end = time.time()
+      print('{} ran in : {} sec'.format(func.__name__, end-start))
+      return result
+    return wrapper
+
+def xor(text, key):
+  return text ^ key
+
+def check_ascii(text):
+  return 32 <= ord(text) <= 122
+
+def decode(text, key):
+  text, key = text, ord(key)
+  char = chr(xor(text, key))
+  if check_ascii(char):
+    return char
+  return 0
+
+with open('p059_cipher.txt', 'r') as f:
+  coded_ascii_text = f.read().split(',')
+coded_ascii_text = [int(i) for i in coded_ascii_text]
+key = [chr(Counter(coded_ascii_text[i::3]).most_common(1)[0][0] ^ 32) for i in range(3)]
+
+@timer
+def main():
+  decoded_text, decoded_text_sum = '', 0
+  for k,text in enumerate(coded_ascii_text):
+    char = decode(text, key[k%3])
+    try:
+      decoded_text += char
+      decoded_text_sum += ord(char)
+    except TypeError:
+      pass
+  return decoded_text, decoded_text_sum
+
+if __name__ == '__main__':
+  decoded_text, decoded_text_sum = main()
+  print(decoded_text)
+  print(key)
+  print(decoded_text_sum)
