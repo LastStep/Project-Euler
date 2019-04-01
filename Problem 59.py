689ddc8c1eef40d4ac7ea213c01418b8bac08587
Rohan Yadav <drohanyadav@gmail.com>
2019-04-01 17:43:52 +0530

    Add files via upload

--- Problem 59.py
+++ Problem 59.py
@@ -23,13 +23,18 @@ def decode(text, key):
     return char
   return 0
 
-with open('p059_cipher.txt', 'r') as f:
-  coded_ascii_text = f.read().split(',')
-coded_ascii_text = [int(i) for i in coded_ascii_text]
-key = [chr(Counter(coded_ascii_text[i::3]).most_common(1)[0][0] ^ 32) for i in range(3)]
+def get_key(coded_ascii_text):
+  return [chr(Counter(coded_ascii_text[i::3]).most_common(1)[0][0] ^ 32) for i in range(3)]
+
+def read_file():
+  with open('p059_cipher.txt', 'r') as f:
+    coded_ascii_text = f.read().split(',')
+  return [int(i) for i in coded_ascii_text]
 
 @timer
 def main():
+  coded_ascii_text = read_file()
+  key = get_key(coded_ascii_text)
   decoded_text, decoded_text_sum = '', 0
   for k,text in enumerate(coded_ascii_text):
     char = decode(text, key[k%3])
@@ -38,10 +43,10 @@ def main():
       decoded_text_sum += ord(char)
     except TypeError:
       pass
-  return decoded_text, decoded_text_sum
+  return decoded_text, decoded_text_sum, key
 
 if __name__ == '__main__':
-  decoded_text, decoded_text_sum = main()
+  decoded_text, decoded_text_sum, key = main()
   print(decoded_text)
   print(key)
   print(decoded_text_sum)
